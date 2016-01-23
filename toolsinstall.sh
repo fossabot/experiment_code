20d52d5e4d207ef8b23186836c6baedb1f135c07
bb eight <156516952@qq.com>
2016-01-23 11:01:56 +0800

    too;

---
+++ toolsinstall.sh
@@ -0,0 +1,17 @@
+sudo apt-get install ibus-rime
+sudo apt-get install docker.io
+sudo apt-get install docker-compose
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get -f install libappindicator1 libindicator7
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo usermod -aG docker bb8
+sudo apt-get install htop
+sudo apt-get install python-pip python-dev build-essential
+curl "https://raw.githubusercontent.com/niasand/cool-config/master/vimrc.sh" -o "vimrc.sh"
+chmod +x vimrc.sh
+./vimrc.sh
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python get-pip.py
+
+
+sudo rm /var/crash/*
