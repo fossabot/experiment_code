6b3acf48ed2ca9d9caca9688bf5a56d4f97fd831
Zhiwei Yang <tencrance@gmail.com>
2017-10-18 14:57:05 +0800

    add zhongzhuanchang

--- pandas_learn/aggregation.py
+++ pandas_learn/aggregation.py
@@ -20,7 +20,9 @@ def main():
     filename = 'phone_data.csv'
     df = get_csv(filename)
     data  = pandas_apply(df)
+    
     print data['item'].count()
+    
     print data['duration'].max()
     print data['duration'][data['item']=='call'].sum() # How many seconds of phone calls are recorded in total?
     print data['month'].value_counts() # How many entries are there for each month?
--- python_tricks/duck_type.py
+++ python_tricks/duck_type.py
@@ -5,15 +5,15 @@
 # @Last Modified time: 2017-09-26 20:07:47
 class Duck:
     def quack(self):
-        print "這鴨子在呱呱叫"
+        print("這鴨子在呱呱叫")
     def feathers(self):
-        print "這鴨子擁有白色與灰色羽毛"
+        print("這鴨子擁有白色與灰色羽毛")
 
 class Person:
     def quack(self):
-        print "這人正在模仿鴨子"
+        print("這人正在模仿鴨子")
     def feathers(self):
-        print "這人在地上拿起1根羽毛然後給其他人看"
+        print("這人在地上拿起1根羽毛然後給其他人看")
 
 def in_the_forest(d):
     d.quack()
