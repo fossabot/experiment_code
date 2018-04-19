266f915e45d37cf25481f8f0eae67a3840ad0901
Zhiwei Yang <tencrance@gmail.com>
2018-04-19 10:17:35 +0800

    add test

---
+++ test_tools/hook_method_unittest.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# @Author: Zhiwei Yang
+# @Date:   2018-04-19 10:14:01
+# @Last Modified by:   Zhiwei Yang
+# @Last Modified time: 2018-04-19 10:17:15
+
+import  unittest
+from selenium import  webdriver
+import time 
+
+class TestDiv(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver=webdriver.Chrome()
+        cls.driver.get('http://www.baidu.com')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+    def test_001(self):
+        self.assertEqual(self.driver.title,u'百度一下，你就知道')
+
+    
+    def test_002(self):
+        self.assertEqual(self.driver.current_url,'https://www.baidu.com/')
+
+    def test_003(self):
+        self.driver.find_element_by_id("kw").send_keys("python")
+        self.driver.find_element_by_id("su").click()
+        time.sleep(3)
+
+
+if __name__=='__main__':
+    suite=unittest.TestSuite(unittest.makeSuite(TestDiv))
+    unittest.TextTestRunner(verbosity=2).run(suite)
